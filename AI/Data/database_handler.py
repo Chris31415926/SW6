824b50acbfca9af7f1cdd584951e10868682712a
Taoshi <nkha20@student.aau.dk>
2023-03-01 13:37:32 +0100

    Add filtered data to array and error handling

--- AI/Data/database_handler.py
+++ AI/Data/database_handler.py
@@ -1,17 +1,26 @@
+import numpy as np
 import sqlite3
+import sys
+import traceback
 
 connection = sqlite3.connect('datasetNY.db')
 
-print('Opened database successfully')
-
 result = connection.execute('SELECT * FROM Motor_Vehicle_Collisions_Crashes')
-count = 0
+filtered = []
 
-for row in result:
-    if row[4] is not None and row[5] is not None:
-        if float(row[4]) > 40.4 and float(row[4]) < 41 and float(row[5]) > -74.3 and float(row[5]) < -73.5:
-            count = count + 1
+try:
+    for row in result:
+        if row[4] is not None and row[5] is not None:
+            if float(row[4]) > 40.4 and float(row[4]) < 41 and float(row[5]) > -74.3 and float(row[5]) < -73.5:
+                filtered.append(row)
+except sqlite3.Error as er:
+    print('SQLite error: %s' % (' '.join(er.args)))
+    print("Exception class is: ", er.__class__)
+    print('SQLite traceback: ')
+    exc_type, exc_value, exc_tb = sys.exc_info()
+    print(traceback.format_exception(exc_type, exc_value, exc_tb))
 
-print('Number of accidents in the area: ', count)
+
+print('Number of accidents in the area: ', len(filtered))
 
 connection.close()
