642b71a64ecb90a39ace073043db6dcf2f7bc54d
Taoshi <nkha20@student.aau.dk>
2023-03-02 10:54:27 +0100

    Filter the database instead of retrieved data

--- AI/Data/database_handler.py
+++ AI/Data/database_handler.py
@@ -4,21 +4,14 @@ import sys
 import traceback
 
 # Function to get the data from the database, filter it and return the filtered data
-def get_data(dbPath):
+def get_all_data(dbPath):
     # Connect to the database
     connection = sqlite3.connect(dbPath)
-
+    raw_data = []
+    
     # Get the data from the database
-    raw_data = connection.execute('SELECT * FROM Motor_Vehicle_Collisions_Crashes')
-    filtered_data = []
-
-    # Filter the data
     try:
-        for row in raw_data:
-            if row[4] is not None and row[5] is not None:
-                # Filter the data to only include the data in the area of interest
-                if 40.4 < float(row[4]) < 41 and -74.3 < float(row[5]) < -73.5:
-                    filtered_data.append(row)
+        raw_data = connection.execute('SELECT * FROM Motor_Vehicle_Collisions_Crashes')
 
     # If there is an error, print the error and traceback                
     except sqlite3.Error as er:
@@ -30,24 +23,17 @@ def get_data(dbPath):
 
     # Close the connection and return the filtered data
     connection.close()
-    return filtered_data
+    return raw_data
 
 # Function to get the data from the database, filter it and return the filtered data
-def get_data(dbPath, amount):
+def get_n_data(dbPath, amount):
     # Connect to the database
     connection = sqlite3.connect(dbPath)
+    raw_data = []
 
     # Get the data from the database
-    raw_data = connection.execute('SELECT * FROM Motor_Vehicle_Collisions_Crashes').fetchmany(amount)
-    filtered_data = []
-
-    # Filter the data
     try:
-        for row in raw_data:
-            if row[4] is not None and row[5] is not None:
-                # Filter the data to only include the data in the area of interest
-                if 40.4 < float(row[4]) < 41 and -74.3 < float(row[5]) < -73.5:
-                    filtered_data.append(row)
+        raw_data = connection.execute('SELECT * FROM Motor_Vehicle_Collisions_Crashes').fetchmany(amount)
 
     # If there is an error, print the error and traceback                
     except sqlite3.Error as er:
@@ -59,4 +45,6 @@ def get_data(dbPath, amount):
 
     # Close the connection and return the filtered data
     connection.close()
-    return filtered_data
+    return raw_data
+
+
